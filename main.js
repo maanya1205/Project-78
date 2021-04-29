e04961fff2c5f8e9ff63ac2b376453bbde19bdf7
maanya1205 <77110811+maanya1205@users.noreply.github.com>
2021-04-29 08:58:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var names=[
+    "Aditya Maheshwari" ,
+    "Sangita Maheshwari",
+     "Stuti Maheshwari and Maanya Maheshwari"
+];
+var images=[
+    "father.jfif" ,
+    "moter.jfif" ,
+      "sisters.jfif"
+];
+var i=0
+function next(){
+    document.getElementById("img1").src=images[i];
+    i++;
+}
